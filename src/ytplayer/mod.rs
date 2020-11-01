7466bbb2ba24e9cf1cf27d2d3005dc6f3f7fc295
Federico Morrone <contact@morrone.dev>
2020-11-01 16:05:43 +0100

    first commit

---
+++ src/ytplayer/mod.rs
@@ -0,0 +1,26 @@
+use serde::{Deserialize, Serialize};
+
+mod streaming_data;
+pub use streaming_data::*;
+mod video_details;
+pub use video_details::*;
+mod captions;
+pub use captions::*;
+
+#[derive(Serialize, Deserialize)]
+pub struct Config {
+    pub args: Args,
+}
+#[derive(Serialize, Deserialize)]
+pub struct Args {
+    pub player_response: PlayerResponse,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct PlayerResponse {
+    #[serde(rename = "streamingData")]
+    pub streaming_data: StreamingData,
+    #[serde(rename = "videoDetails")]
+    pub video_details: VideoDetails,
+    pub captions: Captions,
+}
