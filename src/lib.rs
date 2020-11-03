83565a9f5d9e908f263634fcc03b02d2875af878
Federico Morrone <flickmor@gmail.com>
2020-11-03 21:04:05 +0100

    Added lib.rs and switched to anyhow

---
+++ src/lib.rs
@@ -0,0 +1,42 @@
+use regex::Regex;
+use anyhow::Result;
+
+mod utils;
+pub mod ytplayer;
+
+pub async fn get_info(id: &str) -> Result<ytplayer::Config> {
+    let full = format!(
+        "https://www.youtube.com/watch?v={}&{}&bpctr={}",
+        id,
+        "hl=en",
+        utils::since_epoch()
+    );
+
+    let page = reqwest::get(&full).await?.text().await?;
+
+    let re = Regex::new(r";ytplayer\.config\s*=\s*(\{.+?\});").unwrap();
+    let config = re.captures(&page).unwrap().get(1).unwrap().as_str();
+
+    let config = config
+        .replace("\\\\", "\\")
+        .replace("\\\"", "\"")
+        .replace("}}}}\"}}", "}}}}}}")
+        .replace("\"{\"", "{\"");
+
+    // fs::write("temp/config.json", &config).unwrap();
+    // let config = fs::read_to_string("temp/config.json").unwrap();
+
+    let config: ytplayer::Config = serde_json::from_str(config.as_str()).unwrap();
+
+    Ok(config)
+}
+
+pub async fn download(config: &ytplayer::Config) -> Result<()> {
+    // fs::write(
+    //     "temp/moe.json",
+    //     serde_json::to_string_pretty(&config).unwrap(),
+    // )
+    // .unwrap();
+
+    Ok(())
+}
